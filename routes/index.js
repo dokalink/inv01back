ac5f9b8ee652894de02f638b023d64242bba7df8
Dmitriy Lashkov <admin@dokalink.ru>
2021-04-02 15:37:24 +0300

    /api/rss lenta + mos сокращенная версия

--- routes/index.js
+++ routes/index.js
@@ -10,11 +10,6 @@ router.get('/api/rss', (req, res) => {
   (async () => {
     const rssMos = await parser.parseURL('https://www.mos.ru/rss');
     const rssLenta = await parser.parseURL('https://lenta.ru/rss/news');
-    /*
-    await res.json(rssMos.items.concat(rssLenta.items).sort(
-      (a, b) => new Date(b.isoDate) - new Date(a.isoDate),
-    ));
-    */
     const mas = rssMos.items.concat(rssLenta.items).sort(
       (a, b) => new Date(b.isoDate) - new Date(a.isoDate),
     );
@@ -26,7 +21,6 @@ router.get('/api/rss', (req, res) => {
         img: item.enclosure.url ? item.enclosure.url : '',
         content: item.content ? item.content : '',
         contentSnippet: item.contentSnippet ? item.contentSnippet : '',
-        author: item.author ? item.author : '',
       });
     });
     await res.json(mm);
