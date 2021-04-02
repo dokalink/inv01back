b8f8ab911c8490da7045f40c03764b4103b3e68a
Dmitriy Lashkov <admin@dokalink.ru>
2021-04-02 15:31:00 +0300

    /api/rss lenta + mos сокращенная версия

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,35 @@
 const express = require('express');
-
-const router = express.Router();
-
 const Parser = require('rss-parser');
 
+const router = express.Router();
 const parser = new Parser();
 
-router.get('/api/all', (req, res, next) => {
-  (async () => {
-    const feed = await parser.parseURL('https://www.mos.ru/rss');
-    const feed2 = await parser.parseURL('https://lenta.ru/rss/news');
+// https://inv01back.herokuapp.com/
 
-    await res.json(feed.items.concat(feed2.items).sort(
+router.get('/api/rss', (req, res) => {
+  (async () => {
+    const rssMos = await parser.parseURL('https://www.mos.ru/rss');
+    const rssLenta = await parser.parseURL('https://lenta.ru/rss/news');
+    /*
+    await res.json(rssMos.items.concat(rssLenta.items).sort(
       (a, b) => new Date(b.isoDate) - new Date(a.isoDate),
     ));
+    */
+    const mas = rssMos.items.concat(rssLenta.items).sort(
+      (a, b) => new Date(b.isoDate) - new Date(a.isoDate),
+    );
+    const mm = [];
+    mas.forEach((item) => {
+      mm.push({
+        title: item.title ? item.title : '',
+        link: item.link ? item.link : '',
+        img: item.enclosure.url ? item.enclosure.url : '',
+        content: item.content ? item.content : '',
+        contentSnippet: item.contentSnippet ? item.contentSnippet : '',
+        author: item.author ? item.author : '',
+      });
+    });
+    await res.json(mm);
   })();
 });
 
